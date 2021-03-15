bf57a8bf3f3d8cbc4e3e94b44166e87a3bb18dd2
loulawrence <72574166+loulawrence@users.noreply.github.com>
2021-03-15 15:28:26 -0400

    Add stderr redirection

--- winlog.py
+++ winlog.py
@@ -95,70 +95,11 @@ class logwin32_output:
         os.dup2(to_fd, self.saved_stdout)
         sys.stderr = io.TextIOWrapper(os.fdopen(self.saved_stderr, 'wb'))
 
-class winlog:
-    def __init__(self, logfile, echo ):
-        self.echo = echo
-        self.libc = None
-        self.c_stream = None
-        self.sys_stream = None
-        self.orig_stream_fd = None
-        if logfile is None:
-            self.logfile = tempfile.TemporaryFile(mode='w+b')
-        else:
-            self.logfile = logfile
-        self.saved_sys_stream_fd = None
 
-    def __enter__(self):
-        self.writer = open(self.logfile, mode='wb+')
-        self.writer.write(b'')
-        self.reader = open(self.logfile, mode='rb+')
-        #TEMPORARY: change to both later
-        self.sys_attr = 'stdout'
-        sys_stream = getattr(sys, self.sys_attr)
-        self.set_stream_type()
-        self.orig_sys_stream_fd = sys_stream.fileno()
-        # Save a copy of the original stdout fd in saved_sys_stream_fd
-        self.saved_sys_stream_fd = os.dup(self.orig_sys_stream_fd)
-        # Create a temporary file and redirect stdout to it
-        self.echo_writer = open(os.dup(sys.stdout.fileno()), "w")
-        self._redirect_stream(self.writer.fileno())
-        self.logged_lines = []
-        self._kill = threading.Event()
-        def background_reader(reader, echo_writer, _kill):
-            while True:
-                is_killed = _kill.wait(.1)
-                self.flush()
-                line = reader.readline()
-                while line:
-                    self.logged_lines.append(line)
-                    if self.echo:
-                        self.echo_writer.write('thread write: {}\n'.format(line))
-                        self.echo_writer.flush()
-                    line = reader.readline()
-
-                if is_killed:
-                    break
-
-        self._thread = Thread(target=background_reader, args=(self.reader, self.echo_writer, self._kill))
-        self._thread.start()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print('THREAD: FINISHED REDIRECTION')
-        self.echo_writer.flush()
-        self.flush()
-        self._kill.set()
-        self._thread.join()
-        try:
-            # then redirect stdout back to the saved fd
-            if self.saved_sys_stream_fd is not None:
-                self._redirect_stream(self.saved_sys_stream_fd)
-        finally:
-            if self.saved_sys_stream_fd is not None:
-                os.close(self.saved_sys_stream_fd)
-
-        print('logged_lines = {!r}'.format(self.logged_lines))
-    
-    def set_stream_type(self):
+class StreamWrapper:
+    def __init__(self, sys_attr):
+        self.sys_attr = sys_attr
+        self.saved_stream = None
         if sys.platform.startswith('win32'):
             if sys.version_info < (3, 5):
                 libc = ctypes.CDLL(ctypes.util.find_library('c'))
@@ -185,6 +126,10 @@ class winlog:
             # The original fd stdout points to. Usually 1 on POSIX systems for stdout.
             self.libc = ctypes.CDLL(None)
             self.c_stream = ctypes.c_void_p.in_dll(self.libc, self.sys_attr)
+        self.sys_stream = getattr(sys, self.sys_attr)
+        self.orig_stream_fd = self.sys_stream.fileno()
+        # Save a copy of the original stdout fd in saved_sys_stream_fd
+        self.saved_stream = os.dup(self.orig_stream_fd)
 
     def _redirect_stream(self, to_fd):
         """Redirect stdout to the given file descriptor."""
@@ -198,9 +143,9 @@ class winlog:
         sys_stream.flush()
         sys_stream.close()
         # Make orig_sys_stream_fd point to the same file as to_fd
-        os.dup2(to_fd, self.orig_sys_stream_fd)
+        os.dup2(to_fd, self.orig_stream_fd)
         # Set sys.stdout to a new stream that points to the redirected fd
-        new_buffer = open(self.orig_sys_stream_fd, 'wb')
+        new_buffer = open(self.orig_stream_fd, 'wb')
         new_stream = io.TextIOWrapper(new_buffer)
         setattr(sys, self.sys_attr, new_stream)
         self.sys_stream = getattr(sys, self.sys_attr)
@@ -212,15 +157,77 @@ class winlog:
             self.libc.fflush(self.c_stream)
         self.sys_stream.flush()
 
+    def close(self):
+        try:
+            if self.saved_stream is not None:
+                self._redirect_stream(self.saved_stream)
+        finally:
+            if self.saved_stream is not None:
+                os.close(self.saved_stream)
+
+
+class winlog:
+    def __init__(self, logfile, echo ):
+        self.echo = echo
+        self.logfile = logfile
+        self.stdout = StreamWrapper('stdout')
+        self.stderr = StreamWrapper('stderr')
+
+    def __enter__(self):
+        self.writer = open(self.logfile, mode='wb+')
+        self.writer.write(b'')
+        self.reader = open(self.logfile, mode='rb+')
+        #TEMPORARY: change to both later
+        # Create a temporary file and redirect stdout to it
+        self.echo_writer = open(os.dup(sys.stdout.fileno()), "w")
+        self.stdout._redirect_stream(self.writer.fileno())
+        self.stderr._redirect_stream(self.writer.fileno())
+        self.logged_lines = []
+        self._kill = threading.Event()
+        def background_reader(reader, echo_writer, _kill):
+            while True:
+                is_killed = _kill.wait(.1)
+                self.stdout.flush()
+                self.stderr.flush()
+                line = reader.readline()
+                while line:
+                    self.logged_lines.append(line)
+                    if self.echo:
+                        self.echo_writer.write('thread write: {}\n'.format(line))
+                        self.echo_writer.flush()
+                    line = reader.readline()
+
+                if is_killed:
+                    break
+
+        self._thread = Thread(target=background_reader, args=(self.reader, self.echo_writer, self._kill))
+        self._thread.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('THREAD: FINISHED REDIRECTION')
+        self.echo_writer.flush()
+        self.stdout.flush()
+        self.stderr.flush()
+        self._kill.set()
+        self._thread.join()
+        self.stdout.close()
+        self.stderr.close()
+
+        print('logged_lines = {!r}'.format(self.logged_lines))
+
 if __name__ == '__main__':
     f = 'output.txt'
     with winlog(f, True) as logger:
         print('hello world')
 
         subprocess.run(['cmake', '--version'])
-
+        subprocess.run(['cmake', 'badcommand'])
         subprocess.run(['cmake', '--version'])
+        sys.stderr.write("Some Error!\n")
         print('hello world')
+        #uncomment to confirm __exit__ on failure
+        #raise KeyError("Key Error")
         
     print('finished redirection')
+    sys.stderr.write("Another Error!\n")
     print('finished redirection')
